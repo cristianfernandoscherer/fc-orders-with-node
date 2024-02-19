fba760d551d0a4e1cc1435858131509a221d4993
cristian.scherer <cristianfernandoscherer@gmail.com>
2024-02-19 14:43:21 -0300

    change console.log handler interface

--- src/domain/event/customer/handler/envia-console-log.handler.ts
+++ src/domain/event/customer/handler/envia-console-log.handler.ts
@@ -1,10 +1,10 @@
 import EventHandlerInterface from "../../@shared/event-handler.interface";
-import CustomerCreatedEvent from "../customer-created.event";
+import CustomerChangeAddressEvent from "../customer-change-address.event";
 
 export default class EnviaConsoleLogHandler
-    implements EventHandlerInterface<CustomerCreatedEvent>
+    implements EventHandlerInterface<CustomerChangeAddressEvent>
 {
-    handle(event: CustomerCreatedEvent): void {
+    handle(event: CustomerChangeAddressEvent): void {
         console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} aletardo para: ${event.eventData.address.street}, nro.${event.eventData.address.number}, ${event.eventData.address.city} - ${event.eventData.address.zip}`);
     }
 }
